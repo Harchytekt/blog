fd1be0a90a80f4829ed05ade7d8cb3139c049fcb
Alexandre Ducobu <alexandre.ducobu@yahoo.be>
2021-09-11 18:31:49 +0200

    Fix link tags

--- post.php
+++ post.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Le coin de Ducobu</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="style_grid.css">
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="style_grid.css"/>
 </head>
 <body>
 <header>
